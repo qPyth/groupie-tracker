6d13f41a151535627e3e70c41068c1660a2d8aed
Adil <iskakov921@gmail.com>
2023-10-06 02:16:05 +0600

    save

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	h "groupie-tracker/internal/http"
 	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("asd")
 	log.Fatal(http.ListenAndServe(":3000", h.Routes()))
 }
